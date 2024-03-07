1addd7083bd9cf61699f7d90da69510b9972028c
Coderprince660 <kushwahaprince660@gmail.com>
2024-03-07 21:40:48 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import connectDB from "./config/db";
+import cors from "cors";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
+
+// Routes
+import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes";
+import commentRoutes from "./routes/commentRoutes";
+import postCategoriesRoutes from "./routes/postCategoriesRoutes";
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+//AWS
+const __dirname = path.dirname("")
+const buildpath = path.join(__dirname, "../frontend/build")
+app.use(express.static(buildpath));
+
+app.use(cors({
+  "origin": "*"
+}));
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/post-categories", postCategoriesRoutes);
+
+// static assets
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
